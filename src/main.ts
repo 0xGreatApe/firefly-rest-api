77ccac0244518236a31b8b823fe4578b466c41b6
0xgreatape <0xgreatape@gmail.com>
2024-06-04 14:48:39 +0700

    updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-const { SwaggerTheme, SwaggerThemeNameEnum } = require('swagger-themes');
-
+import * as express from 'express';
+import { join } from 'path';
 import { AppModule } from './app.module';
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json'); // Your Swagger JSON file
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Serve Swagger UI static files
+  app.use(
+    '/swagger-ui',
+    express.static(join(__dirname, '..', 'node_modules/swagger-ui-dist')),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Firefly Pricefeed API')
@@ -16,13 +24,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  const theme = new SwaggerTheme();
-  const options = {
-    explorer: true,
-    customCss: theme.getBuffer(SwaggerThemeNameEnum.DRACULA),
-  };
-
-  SwaggerModule.setup('/', app, document, options);
+  SwaggerModule.setup('/api-docs', app, document);
 
   await app.listen(process.env.PORT || 3000);
 }
